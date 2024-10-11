6be6e06a664105326a8227b37141025c2e25e2c8
unknown <zaidkhan262523@gmil.com>
2024-10-12 01:01:13 +0530

    now completed

--- src/Components/Footer.js
+++ src/Components/Footer.js
@@ -1,7 +1,7 @@
 import React from "react";
 function Footer() {
   return (
-    <footer className="container mx-auto py-1 fixed bottom-0 md:left-20 bg-white dark:bg-dark-mode">
+    <footer className="container mx-auto py-1 fixed bottom-0 mt-[40px] md:left-20 bg-white dark:bg-dark-mode">
       <p className="text-xs text-center text-dark-content dark:text-light-content w-full">
         Designed and Coded by{" "}
         <a
