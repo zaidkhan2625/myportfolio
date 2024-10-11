7f91bec1a503b13025acf02db7661f0018ef67f1
unknown <zaidkhan262523@gmil.com>
2024-10-12 00:59:11 +0530

    now completed

--- src/App.js
+++ src/App.js
@@ -14,7 +14,7 @@ function App() {
         <Route path="/" element={<LandingPage />} />
         <Route path="/about" element={<About />} />
         <Route path="/contact" element={<Contact />} />
-        {/* <Route path="/projects" element={<Projects />} /> */}
+        <Route path="/projects" element={<Projects />} />
         <Route path="/technologies" element={<Technologies />} />
       </Routes>
       <Footer />
--- src/Pages/LandingPage.js
+++ src/Pages/LandingPage.js
@@ -4,14 +4,37 @@ import About from './About';
 import Technologies from './Technologies';
 import Project from '../Components/Project';
 import Contact from './Contact';
-  
+import { projectDetails } from "../Details";
+
   const LandingPage = () =>  {
 	return (
 	  <div>
       <Home/>
       <About/>
       <Technologies/>
-      <Project/>
+      <main className="container mx-auto max-width pt-10 mb-20">
+      <section>
+        <h1 className="text-2xl text-dark-heading dark:text-light-heading md:text-4xl xl:text-5xl xl:leading-tight font-bold">
+          Projects
+        </h1>
+        <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-x-10">
+          {React.Children.toArray(
+            projectDetails.map(
+              ({ title, image, description, techstack, previewLink, githubLink }) => (
+                <Project
+                  title={title}
+                  image={image}
+                  description={description}
+                  techstack={techstack}
+                  previewLink={previewLink}
+                  githubLink={githubLink}
+                />
+              )
+            )
+          )}
+        </div>
+      </section>
+    </main>
       <Contact/>
 	  </div>
 	);
